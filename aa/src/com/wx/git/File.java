a5104cc4e4d66952019d6d62f3ad7f75f63bd5ab
admin <lty926498@163.com>
2020-08-16 17:59:46 +0800

    aaa

---
+++ aa/src/com/wx/git/File.java
@@ -0,0 +1,19 @@
+package com.wx.git;
+
+import org.junit.Test;
+
+public class File {
+
+    @Test
+    public void aa(){
+        System.out.println("aaa!");
+    }
+    @Test
+    public void bb(){
+        System.out.println("bbb!");
+    }
+    @Test
+    public void cc(){
+        System.out.println("ccc!");
+    }
+}
